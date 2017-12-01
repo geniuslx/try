42eb304f86d1739d2baf97a7ea0dc96bbfdbfe4f
Linxue Zhao <geniuslx92@gmail.com>
2017-12-01 11:36:54 +1000

    Print my name several times because I like it

--- myfirstfile.go
+++ myfirstfile.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Linxue says Hello\n\n\n")
+	fmt.Printf("Linxue says Hello\n\n\n")
+	fmt.Printf("Linxue says Hello\n\n\n")
+	fmt.Printf("Linxue says Hello\n\n\n")
 }
